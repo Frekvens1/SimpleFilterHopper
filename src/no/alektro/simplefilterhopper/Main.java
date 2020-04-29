3f833418071d7e67c0f0ef307fc01c65de274820
Frekvens1 <Frekvens1@gmail.com>
2020-04-29 20:11:31 +0200

    Create Main.java

---
+++ src/no/alektro/simplefilterhopper/Main.java
@@ -0,0 +1,12 @@
+package no.alektro.simplefilterhopper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable()
+	{        
+		getServer().getPluginManager().registerEvents(new EventListener(), this);
+	}
+}
